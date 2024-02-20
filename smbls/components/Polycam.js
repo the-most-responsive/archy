b80de345b076cd73a74db98517dade28ece5ba5a
nikoloza <nika.tomadze@gmail.com>
2024-02-21 02:16:57 +0400

    added 3d

--- smbls/components/Polycam.js
+++ smbls/components/Polycam.js
@@ -5,19 +5,21 @@ export const Polycam = {
     position: 'relative'
   },
 
-  Iframe: (el, state) => ({
-    background: 'white .05',
-    src: 'https://poly.cam/capture/195D64EF-24DB-4F68-A3ED-489CBB18D278',
-    allowfullscreen: '',
-    loading: 'lazy',
-    title: 'polycam capture viewer',
-    referrerpolicy: 'no-referrer-when-downgrade',
-    flex: '1',
-    minWidth: '0',
-    minHeight: 'I',
-    width: '100%',
-    border: '0'
-  }),
+  Iframe: {
+    props: (el, state) => ({
+      background: 'white .05',
+      src: state.view === '360' ? 'https://poly.cam/capture/0AF39C3B-E953-426F-8AF5-82284C113346' : state.view === 'room' ? 'https://poly.cam/capture/9E19E3FA-DB29-4F79-BE22-EC8CAA4FFA09' : 'https://poly.cam/capture/1FAA00D4-032D-4030-AA17-28B87C27A8BC',
+      allowfullscreen: '',
+      loading: 'lazy',
+      title: 'polycam capture viewer',
+      referrerpolicy: 'no-referrer-when-downgrade',
+      flex: '1',
+      minWidth: '0',
+      minHeight: 'I',
+      width: '100%',
+      border: '0'
+    })
+  },
 
   Switcher: {
     props: {
@@ -40,11 +42,12 @@ export const Polycam = {
         '.active': {
           theme: 'secondary'
         },
-        onClick: (ev, el, s) => s.update({ view: el.key })
+        onClick: (ev, el, s) => s.update({ view: el.key !== s.view ? el.key : null })
       })
     },
 
     360: { text: '360°' },
-    '3d-mockup': { text: '3d mockup' }
+    '3d-mockup': { text: '3d mockup' },
+    room: { text: 'room' }
   }
 }
